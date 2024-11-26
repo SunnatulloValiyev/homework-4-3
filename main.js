54f51b96355b858699635fb237082ce7a11e5ad6
SunnatulloValiyev <sunnatillovaliyev769@gmail.com>
2024-11-26 23:26:01 +0500

    change

---
+++ main.js
@@ -0,0 +1,467 @@
+// 1-misol
+
+// let num1 = +prompt("Son kiriting:")
+// let num2 = +prompt("Son kiriting:")
+
+// if (num1 && num2 > 0 ) {
+//     console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+
+
+// 2-misol
+
+// let num1 = +prompt("Son kiriting:")
+// let num2 = +prompt("Son kiriting:")
+// let num3 = +prompt("Son kiriting:")
+
+// if (num1 < 0 || num2 < 0 || num3 < 0) {
+//     console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+
+// 3-misol
+
+// let num = +prompt("Son kiriting")
+
+// if (num > 0 && num % 2 == 0) {
+//     console.log("musbat son va juft son")
+// }
+// else{
+//     console.log("musbat son ham va juft son ham emas");
+// }
+
+// 4-misol
+
+// let num1 = +prompt("Son kirting")
+// let num2 = +prompt("Son kirting")
+
+// let res1 = num1 + num2;
+// let res2 = num1 * num2;
+
+// if (res1 > res2){
+//     console.log("yigindilar kotta");
+// }
+// else if (res1 < res2){
+//     console.log("kopaytma yigindidan kotta");
+// }
+
+// 5-misol
+
+// let num = +prompt("Son kiriting:")
+
+// if(num > 100 && num < 999){
+//     console.log("Son 100 dan katta va 999 dan kichik");
+// }
+// else{
+//     console.log("son ikkalasiga ham to'gri kelmaydi");
+// }
+
+// 6-misol
+
+// let num1 = +prompt("Son kiriting:")
+// let num2 = +prompt("Son kiriting:")
+// let num3 = +prompt("Son kiriting:")
+
+// let res = num1 + num2 + num3;
+
+// if (res > 0){
+//     console.log("uch sonning yigindisi musbat");
+// }
+// else {
+//     console.log("uch sonning yigindisi musbat emas");
+// }
+
+// 7-misol
+
+// let num1 = +prompt("Son kiriting:")
+// let num2 = +prompt("Son kiriting:")
+
+// if (num1 !== 0 && num2 !== 0) {
+//     console.log("kiritgan sonda nol yoq");
+// }
+// else{
+//     console.log("kiritgan sonda nol bor");
+// }
+
+// 8-misol
+
+// let num1 = +prompt("Son kiriting:")
+// let num2 = +prompt("Son kiriting:")
+
+// let res1 = num1 * num2;
+// let res2 = num1 - num2;
+
+// if (res1 > 0){
+//     console.log("musbat son");
+// }
+// else{
+//     console.log("musbat emas");
+// }
+
+// if (res2 < 0 || res2 > 0) {
+//     console.log("ayirmasi manfiy son");
+// }
+// else {
+//     console.log("manfiy son emas");
+// }
+
+// 9-misol
+
+// let num = +prompt("Son kiriting:")
+
+// if (numb > 0) {
+//     console.log("Son musbat");
+// }
+// if (num >= 10 && num <= 99) {
+//     console.log("Son ikki xonali");
+// }
+
+// 10-misol
+
+// let number = +prompt("Son kiriting");
+// if (number % 2 === 0 && number % 5 === 0) {
+//     console.log("Son juft va 5 ga karrali.");
+// } else {
+//     console.log("bajarilmadi.");
+// }
+
+// 11-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+// let c = +prompt("Uchinchi sonni kiriting:");
+
+// if (a === b || b === c || a === c) {
+//     alert("Hech bo'lmaganda ikkita son teng.");
+// } else {
+//     alert("Hech bo'lmaganda ikkita son teng emas.");
+// }
+
+
+// 12-misol
+
+// let number = +prompt("Sonni kiriting:");
+// if (number % 7 === 0 && number % 3 !== 0) {
+//     alert("Son 7 ga bo'linadi va 3 ga bo'linmaydi.");
+// } else {
+//     alert("Son 7 ga bo'linadi yoki 3 ga bo'linadi.");
+// }
+
+
+// 13-misol
+
+// let x = +prompt("Birinchi sonni kiriting:");
+// let y = +prompt("Ikkinchi sonni kiriting:");
+
+// if (x === 0 || y === 0 || (x < 0 && y < 0)) {
+//     alert("Birinchi yoki ikkinchi son 0 yoki ikkalasi ham manfiy.");
+// } else {
+//     alert("Shart bajarilmadi.");
+// }
+
+// 14-misol
+
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+// let c = +prompt("Uchinchi sonni kiriting:");
+
+// let max = Math.max(a, b, c);
+// if (max > 0) {
+//     alert("Eng katta son musbat.");
+// } else {
+//     alert("Eng katta son musbat emas.");
+// }
+
+// 15-misol
+
+
+// let number = +prompt("Sonni kiriting:");
+// if (!(number >= 10 && number <= 20)) {
+//     alert("Son 10 va 20 orasida emas.");
+// } else {
+//     alert("Son 10 va 20 orasida.");
+// }
+
+
+// 16-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+
+// if (a + b < 0 || a * b > 0) {
+//     alert("Shart bajarildi.");
+// } else {
+//     alert("Shart bajarilmadi.");
+// }
+
+
+// 17-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+// let c = +prompt("Uchinchi sonni kiriting:");
+
+// let min = Math.min(a, b, c);
+// if (min % 2 === 0) {
+//     alert("Eng kichik son juft.");
+// } else {
+//     alert("Eng kichik son juft emas.");
+// }
+
+
+// 18-misol
+
+// let number = +prompt("Sonni kiriting:");
+// let num = Math.abs(number) % 10;
+// if (num === 5 || num === 0) {
+//     alert("Sonning oxirgi raqami 5 yoki 0.");
+// } else {
+//     alert("Sonning oxirgi raqami 5 yoki 0 emas.");
+// }
+
+
+// 19-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+
+// if ((a > 0 && b < 0) || (a < 0 && b > 0)) {
+//     alert("Birinchi son musbat va ikkinchisi manfiy.");
+// } else {
+//     alert("Birinchi son musbat yoki ikkinchisi manfiy emas.");
+// }
+
+
+// 20-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+// let c = +prompt("Uchinchi sonni kiriting:");
+
+// if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+//     alert("Hech bo'lmaganda bittasi juft.");
+// } else {
+//     alert("Barchasi toq.");
+// }
+
+
+// 22-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+
+// if (a % b === 0 || b % a === 0) {
+//     alert("Birinchi son ikkinchisiga karrali.");
+// } else {
+//     alert("Birinchi son ikkinchisiga karrali emas.");
+// }
+
+// 23-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+// let c = +prompt("Uchinchi sonni kiriting:");
+
+// if (a + b + c === 0 || a * b * c < 0) {
+//     console.log("Yig'indisi 0 yoki ko'paytmasi manfiy.");
+// } else {
+//     console.log("Shart bajarilmadi.");
+// }
+
+
+// 24-misol
+
+// let number = +prompt("Sonni kiriting:");
+
+// if (number % 3 === 0 && number % 4 === 0 && number % 12 !== 0) {
+//     console.log("Son 3 ga va 4 ga bo'linadi, lekin 12 ga bo'linmaydi.");
+// } 
+// else {
+//     console.log("Shart bajarilmadi.");
+// }
+
+
+// 25-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+
+// if ((a % 2 === 0 && b % 2 !== 0) || (a % 2 !== 0 && b % 2 === 0)) {
+//     console.log("Birinchi son juft va ikkinchisi toq.");
+// } else {
+//     console.log("Birinchi son juft yoki ikkinchisi toq emas.");
+// }
+
+
+// 26-misol
+
+// let number = +prompt("Sonni kiriting:");
+// if (number > 0 && (number < 100 || number > 999)) {
+//     console.log("Son musbat va uch xonali emas.");
+// } else {
+//     console.log("Son uch xonali yoki manfiy.");
+// }
+
+
+// 27-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+// let c = +prompt("Uchinchi sonni kiriting:");
+
+// if (a + b + c > 100 || a * b * c < 0) {
+//     console.log("Yig'indisi 100 dan katta yoki ko'paytmasi manfiy.");
+// }
+//  else {
+//     console.log("Shart bajarilmadi.");
+// }
+
+
+// 28-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+
+// if ((a > 10 && b < 0) || (a < 0 && b > 10)) {
+//     console.log("Birinchi son 10 dan katta va ikkinchisi 0 dan kichik.");
+// } else {
+//     console.log("Shart bajarilmadi.");
+// }
+
+
+// 29-misol
+
+// let number =+prompt("Sonni kiriting:");
+// if (number > 0 && number % 2 !== 0) {
+//     console.log("Son toq va musbat.");
+// } else {
+//     console.log("Son toq yoki musbat emas.");
+// }
+
+
+// 30-misol
+
+// let number = +prompt("Sonni kiriting:");
+
+
+// if (number > 0 && number % 2 !== 0) {
+//     console.log("Son toq va musbat.");
+// } else {
+//     console.log("Son toq yoki musbat emas.");
+// }
+
+
+// 31-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+
+// if (a > 0 || b > 0) {
+//     console.log("Hech bo'lmaganda bittasi musbat.");
+// } else {
+//     console.log("Hech bo'lmaganda biri musbat emas.");
+// }
+
+
+// 32-misol
+
+// let number = +prompt("Sonni kiriting:");
+
+
+// if (number % 2 === 0 && number % 4 === 0 && number % 8 !== 0) {
+//     console.log("Son juft va 4 ga karrali, lekin 8 ga karrali emas.");
+// } else {
+//     console.log("Shart bajarilmadi.");
+// }
+
+
+// 34-misol
+
+// let number = +prompt("Sonni kiriting:");
+
+
+// if (number % 2 === 0 && number % 5 === 0 && number % 10 !== 0) {
+//     console.log("Son 2 ga va 5 ga bo'linadi, lekin 10 ga bo'linmaydi.");
+// } else {
+//     console.log("Shart bajarilmadi.");
+// }
+
+
+// 35-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+
+// if (a + b > Math.abs(a - b)) {
+//     console.log("Yig'indisi farqidan katta.");
+// } else {
+//     console.log("Yig'indisi farqidan kichik yoki teng.");
+// }
+
+
+// 36-misol
+
+// let number = +prompt("Sonni kiriting:");
+
+// if (number > 0 && number < 1000) {
+//     console.log("Son 0 dan katta va 1000 dan kichik.");
+// }
+// else {
+//     console.log("Son shartga mos kelmaydi.");
+// }
+
+// 37-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+
+// if ((a % 2 === 0 || b % 2 === 0) && a * b > 20) {
+//     console.log("Birinchi son juft va ko'paytmasi 20 dan katta.");
+// } else {
+//     console.log("Shart bajarilmadi.");
+// }
+
+
+// 38-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+// let c = +prompt("Uchinchi sonni kiriting:");
+
+// if (a % 10 === 0 || b % 10 === 0 || c % 10 === 0) {
+//     console.log("Hech bo'lmaganda bittasi 10 ga bo'linadi.");
+// }
+// else {
+//     console.log("Hech bo'lmaganda biri 10 ga bo'linmaydi.");
+// }
+
+// 39-misol
+
+// let number = +prompt("Sonni kiriting:");
+
+// if (number % 4 === 0 && number % 2 !== 0) {
+//     console.log("Son 4 ga karrali va toq emas.");
+// } 
+// else {
+//     console.log("Shart bajarilmadi.");
+// }
+
+
+// 40-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");
+// let c = +prompt("Uchinchi sonni kiriting:");
+
+// if (a + b > c || a + c > b || b + c > a) {
+//     console.log("Ikki sonning yig'indisi uchinchisidan katta.");
+// } 
+// else {
+//     console.log("Shart bajarilmadi.");
+// }
